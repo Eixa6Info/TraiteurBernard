255bf0c59cb6c80bfbf8404b525570ad13fb24bc
Willy <willy.tardy@outlook.com>
2020-02-21 09:08:53 +0100

    modification DAO (temporaire a revoir)

--- WpfApp1/DAO/MenuDAO.cs
+++ WpfApp1/DAO/MenuDAO.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
-
+using TraiteurBernardWPF.Data;
 using TraiteurBernardWPF.Modele;
 
 namespace TraiteurBernardWPF.DAO
@@ -10,7 +11,25 @@ namespace TraiteurBernardWPF.DAO
     {
         internal static List<Menu> getAllFromWeek(int semaine)
         {
-            return new List<Menu>();
+            BaseContext bd = new BaseContext();
+            //this.db.Entry(p).Reference(s => s.Tournee).Load();
+            IQueryable<Menu> req = from t in bd.Menu
+                                   where t.Semaine == semaine
+                                       select t;
+
+            List<Menu> data = new List<Menu>();
+
+            foreach (Menu p in req)
+            {
+                //Chargement préalable des données liées, sinon "lazy loading"
+                // https://docs.microsoft.com/fr-fr/ef/ef6/querying/related-data
+                // voir pour plus de détails 
+                bd.Entry(p).Collection(s => s.Plats).Load();
+
+
+                data.Add(p);
+            }
+            return data;
         }
     }
 }
