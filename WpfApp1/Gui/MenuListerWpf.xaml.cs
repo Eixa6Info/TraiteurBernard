551ac71656185b502fa0ac19edcc8d25610db7ad
Willy <willy.tardy@outlook.com>
2020-02-21 10:55:27 +0100

    lister les menus fonctionnel
    amélioration de la visualisation des menus (par colonnes et non plus par ligne)

--- WpfApp1/Gui/MenuListerWpf.xaml.cs
+++ WpfApp1/Gui/MenuListerWpf.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using TraiteurBernardWPF.DAO;
 using TraiteurBernardWPF.Data;
+using TraiteurBernardWPF.Modele;
 
 namespace TraiteurBernardWPF.Gui
 {
@@ -52,24 +53,57 @@ namespace TraiteurBernardWPF.Gui
         /// <param name="e"></param>
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
-            // Utilisation du namespace car la classe Menu existe déjà nativement en C#
-            IQueryable<TraiteurBernardWPF.Modele.Menu> req = from t in this.db.Menu
-                                          select t;
 
-            List<List<TraiteurBernardWPF.Modele.Menu>> data = new List<List<TraiteurBernardWPF.Modele.Menu>>();
+            // Liste d'objets anonymes qui vont etre utilisés pour faire une ligne dans la datagrid
+            // forme : Jour, EntreeMidi, Plat1Midi, Plat2Midi, Plat3Midi, DessertMidi, EntreeSoir, PlatSoir, DessertSoir
+            List<object> rowFormList = new List<object>();
 
-            for (int i = 1; i < 10; i++)
+            // Liste d'objets anonymes permettant de mettre en forme l'expander ainsi que la datagrid associée
+            // Contient le Header ainsi que la liste des lignes (rowFormList)
+            List<object> data = new List<object>();
+
+            // Pour les 54 semaines de l'année
+            for (int i = 1; i < 55; i++)
             {
-                data.Add(MenuDao.getAllFromWeek(i));
+                // On récupère tous les menus sur la semaine en itération
+                List<TraiteurBernardWPF.Modele.Menu> listeMenus = MenuDao.getAllFromWeek(i);
+
+                foreach(TraiteurBernardWPF.Modele.Menu menu in listeMenus)
+                {
+
+                    // Tableau qui représente les 7 plats hypothétique de la semaine
+                    Plat[] tabPlats = new Plat[7];
+
+                    // Conversion de l'hashset des plats en tableau puis copie dans le tableau précédent
+                    menu.Plats.CopyTo(tabPlats);
+
+                    // Objet anonyme qui correspond aux données qui vont être bind dans la datagrid
+                    object rowForm = new
+                    {
+                        Jour = menu.Jour,
+                        EntreeMidi = tabPlats[0] != null ? tabPlats[0].Name : "vide",
+                        Plat1Midi = tabPlats[1] != null ? tabPlats[1].Name : "vide",
+                        Plat2Midi = tabPlats[2] != null ? tabPlats[2].Name : "vide",
+                        Plat3Midi = tabPlats[3] != null ? tabPlats[3].Name : "vide",
+                        DessertMidi = tabPlats[4] != null ? tabPlats[4].Name : "vide",
+                        EntreeSoir = tabPlats[5] != null ? tabPlats[5].Name : "vide",
+                        PlatSoir = tabPlats[6] != null ? tabPlats[6].Name : "vide",
+                        DessertSoir = tabPlats[7] !=null ? tabPlats[7].Name : "vide",
+                    };
+
+                    rowFormList.Add(rowForm);
+                }
+
+                // Finalement, pour chaque semaine on ajoute le header correspondant, la liste des menus correspondant
+                // et on remet le tout à 0
+                data.Add(new { Header = $"Semaine {i} | Menus {rowFormList.Count}", Plats = rowFormList });
+                rowFormList = new List<object>();
+
             }
 
             dataGridTournees.ItemsSource = data;
 
         }
 
-        private void DataGridTemplateColumn_SourceUpdated(object sender, DataTransferEventArgs e)
-        {
-
-        }
     }
 }
