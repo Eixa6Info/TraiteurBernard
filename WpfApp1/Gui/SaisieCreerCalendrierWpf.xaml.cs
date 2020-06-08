9d3739ef98b2bf64b0587e2405cba2dc1743b0a8
Eixa6 <contact@eixa6.com>
2020-06-08 15:51:56 +0200

    modification calendrier

--- WpfApp1/Gui/SaisieCreerCalendrierWpf.xaml.cs
+++ WpfApp1/Gui/SaisieCreerCalendrierWpf.xaml.cs
@@ -17,6 +17,8 @@ using TraiteurBernardWPF.Modele;
 using GregorianCalendar = System.Globalization.GregorianCalendar;
 using Calendar = System.Globalization.Calendar;
 using System.IO;
+using TraiteurBernardWPF.Utils;
+using TraiteurBernardWPF.DAO;
 
 namespace TraiteurBernardWPF.Gui
 {
@@ -28,6 +30,7 @@ namespace TraiteurBernardWPF.Gui
         private BaseContext db;
         private Saisie Edite { get; set; }
         private int[] IDs;
+        CalenderBackground background;
 
         public SaisieCreerCalendrierWpf(Saisie edite, BaseContext db, int[] IDs)
         {
@@ -40,127 +43,67 @@ namespace TraiteurBernardWPF.Gui
 
         private void calendar_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
-            Console.WriteLine("la date");
+            
         }
 
         private void Window_Closed(object sender, EventArgs e)
         {
-            Console.WriteLine("avant visibility.Hidden");
             this.Visibility = Visibility.Hidden;
-            Console.WriteLine("apres visibility.Hidden");
         }
 
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             try
             {
+
+                calendar.IsTodayHighlighted = false;
+                background = new CalenderBackground(calendar);
+                background.AddOverlay("circle", "C:\\eixa6\\imgCalendar\\circle.png");
+                calendar.SelectedDates.Clear();
+                background.ClearDates();
                
-                List<int> jour = new List<int>();
-                List<int> numSemaine = new List<int>();
-                List<int> annee = new List<int>();
-                List<string> jourLivraison = new List<string>();
-                List<string> jourRepas1 = new List<string>();
-                List<string> jourRepas2 = new List<string>();
-                List<string> jourRepas3 = new List<string>();
-                int intAnnee = 0;
-                int intMois = 0;
-                int resMois = 0;
-                int intJour = 0;
-
-
+                string resJour = "";
+                int resMois;
                 IQueryable<Saisie> req = from t in db.Saisies
+                                         where
+                                         t.Personne.ID == Edite.Personne.ID
                                          select t;
 
                 IQueryable<Livraison> reqLiv = from t in db.Livraisons
                                                select t;
 
-                foreach (Livraison t in reqLiv)
-                {
-                    jourLivraison.Add(t.JourLivraison);
-                    jourRepas1.Add(t.JourRepas1);
-                    jourRepas2.Add(t.JourRepas2);
-                    jourRepas3.Add(t.JourRepas3);
-                }
-
-
-
+               
                 foreach (Saisie p in req)
                 {
-                    if (p.Personne.ID == Edite.ID)
+                    List<int> reqJourDeSaisie = SaisieDAO.SaisiePourUneJournee(db, Edite.Personne, p.Annee, p.Semaine, p.Jour);
+                    // on calcule la sommes dans la liste
+                    if (reqJourDeSaisie.Sum() > 0)
                     {
-                        jour.Add(p.Jour);
-                        annee.Add(p.Annee);
-                        numSemaine.Add(p.Semaine);
-                        // jourLivraison.Add(Int32.Parse(p.Personne.Tournee.JoursLivraisonsRepas.ToString()));
 
-                        foreach (int aAnnee in annee)
-                        {
-                            intAnnee = aAnnee;
+                        // Afficher sur le calendrier les jours de saisie.
+                        resJour = GestionDeDateCalendrier.LeJourSuivantLeNuméro(p.Jour);
+                        DateTime JourDeSaisie = GestionDeDateCalendrier.TrouverDateAvecNumJourEtNumSemaine(p.Annee, p.Semaine, resJour);
+                        resMois = GestionDeDateCalendrier.TrouverLeMoisAvecNumSemaine(p.Semaine, p.Annee);
 
-                            foreach (int aSemaine in numSemaine)
-                            {
-                                intMois = aSemaine;
-                                Calendar toMoisCalendar = new GregorianCalendar();
+                        calendar.SelectedDates.Add(JourDeSaisie);
+                        calendar.DisplayDate = new DateTime(p.Annee, resMois, 1);
 
-                                int searchedWeek = intMois; // N° de semaien à rechercher
-                                DateTime dtFirstDayFirstWeek = DateTime.MinValue; // pour éviter erreur de compil
-                                for (int iWeek = 0, iDay = 1; iWeek != 1; iDay++)
-                                {
-                                    dtFirstDayFirstWeek = new DateTime(intAnnee, 1, iDay);
-                                    iWeek = toMoisCalendar.GetWeekOfYear(dtFirstDayFirstWeek, System.Globalization.CalendarWeekRule.FirstFourDayWeek, DayOfWeek.Monday);
-                                }
-                                DateTime firstDayOfGivenWeek = toMoisCalendar.AddWeeks(dtFirstDayFirstWeek, searchedWeek - 1);
-                                resMois = firstDayOfGivenWeek.Month;
+                        // Afficher sur le calendrier les jours de livraison par rapport au saisie
+                        DateTime leJourDeLivraison = LivraisonDAO.JourDeLivraisonCal(db, p.Tournee.Nom, p.Annee, p.Semaine, JourDeSaisie);
 
-                                foreach (int aJour in jour)
-                                {
-                                    if (aJour == 1)
-                                    {
-                                        intJour = firstDayOfGivenWeek.Day;
-                                    }
-                                    if (aJour == 2)
-                                    {
-                                        intJour = firstDayOfGivenWeek.Day + 1;
-                                    }
-                                    if (aJour == 3)
-                                    {
-                                        intJour = firstDayOfGivenWeek.Day + 2;
-                                    }
-                                    if (aJour == 4)
-                                    {
-                                        intJour = firstDayOfGivenWeek.Day + 3;
-                                    }
-                                    if (aJour == 5)
-                                    {
-                                        intJour = firstDayOfGivenWeek.Day + 4;
-                                    }
-                                    if (aJour == 6)
-                                    {
-                                        intJour = firstDayOfGivenWeek.Day + 5;
-                                    }
-                                    if (aJour == 7)
-                                    {
-                                        intJour = firstDayOfGivenWeek.Day + 6;
-                                    }
-                                }
-                            }
-                        }
-                        if (intAnnee != 0 && resMois != 0 && intJour != 0)
-                        {
+                        int j = leJourDeLivraison.Day;
+                        int m = leJourDeLivraison.Month;
+                        int y = leJourDeLivraison.Year;
 
-                            Console.WriteLine("annee: " + intAnnee);
-                            Console.WriteLine("mois: " + resMois);
-                            Console.WriteLine("jour: " + intJour);
-                            Console.WriteLine("jour de livraison: ");
-                            Console.WriteLine("JoursDeLivraisonsrepas" + p.Tournee.JoursLivraisonsRepas);
-                            //calendar.BlackoutDates.Add(new CalendarDateRange(new DateTime(intAnnee, resMois, intJour)));
-                            calendar.SelectedDates.Add(new DateTime(intAnnee, resMois, intJour));
-                            calendar.BlackoutDates.Add(new CalendarDateRange(new DateTime()));
-                            calendar.DisplayDate = new DateTime(intAnnee, resMois, intJour);
+                        background.AddDate(new DateTime(y, m, j), "circle");
 
+                        calendar.Background = background.GetBackground();
 
-                        }
-
+                        calendar.DisplayDateChanged += CalenderOnDisplayDateChanged;
+                    }
+                    else
+                    {
+                        Console.WriteLine("il y a pas de jour de saisie");
                     }
                 }
             }
@@ -169,6 +112,10 @@ namespace TraiteurBernardWPF.Gui
                 LogHelper.WriteToFile(a.Message, "PersonneListerWpf.xaml.cs");
                 throw a;
             }
+        }
+        private void CalenderOnDisplayDateChanged(object sender, CalendarDateChangedEventArgs calendarDateChangedEventArgs)
+        {
+            calendar.Background = background.GetBackground();
         }
     }
 }
